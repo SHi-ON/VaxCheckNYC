b4045833ed83ebf24df4aa6c3d4ce00c2a8dbceb
Shayan Amani <7shayan7@gmail.com>
2021-04-09 09:35:27 -0400

    Standard lib only

--- vax_check.py
+++ vax_check.py
@@ -1,19 +1,18 @@
+import json
 import smtplib
 import time
 import webbrowser
 from datetime import datetime
 from email.message import EmailMessage
-
-import requests
+from urllib.request import urlopen
 
 import config
 
+# Javits Center both Pfizer and J&J
+PROVIDER_IDS = [1000, 1019]
 URL = 'https://am-i-eligible.covid19vaccine.health.ny.gov/api/list-providers'
 URL_PRESCREENER = 'https://am-i-eligible.covid19vaccine.health.ny.gov/Public' \
                   '/prescreener '
-
-# Javits Center both Pfizer and J&J
-PROVIDER_IDS = [1000, 1019]
 
 
 def send_email(provider_name, vaccine_brand):
@@ -31,10 +30,11 @@ def send_email(provider_name, vaccine_brand):
 
 
 def check(provider_ids):
-    r = requests.get(URL)
-    providers = r.json()
+    response = urlopen(URL)
+    response_json = json.load(response)
 
-    for provider in providers['providerList']:
+    providers = response_json['providerList']
+    for provider in providers:
         if provider['providerId'] in provider_ids:
             available_apts = provider['availableAppointments']
             if available_apts not in ['NAC', 'N']:
